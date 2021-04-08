a9d25e383beb0a33e9f0a4abc9556fb39b1bdf3e
Jiseung Jung <roadrunner23@yonsei.ac.kr>
2021-04-08 21:46:40 +0900

    created liquid and adding reagent

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,8 @@
 class Reagent {
-    constructor(name, color) {
+    constructor(name, color, detection) {
         this.name = name;
         this.create(color);
+        this.detection = detection;
     }
 
     create(color) {
@@ -39,9 +40,44 @@ class Reagent {
     }
 }
 
-const thymolBlue = new Reagent("Thymol Blue", "#A87A21");
-const methylOrange = new Reagent("Meathyl Orange", "orange");
-const wrightStein = new Reagent("Wright Stein", "blue");
-const btb = new Reagent("Bromothymol Blue", "green");
-const ppt = new Reagent("Phenolphthalein", "white");
-const phonelRed = new Reagent("Phenol Red", "red")
+const thymolBlue = new Reagent("Thymol Blue", "#A87A21", 0.4);
+const methylOrange = new Reagent("Meathyl Orange", "orange", 0.4);
+const wrightStein = new Reagent("Wright Stein", "blue", 0.8);
+const btb = new Reagent("Bromothymol Blue", "green", 0.9);
+const ppt = new Reagent("Phenolphthalein", "white", 0.2);
+const phenolRed = new Reagent("Phenol Red", "red", 0.3);
+
+class Liquid {
+    constructor(min, max) {
+        this.numMicroPlastic = Math.floor((max - min) * Math.random() + min);
+        this.visibileMicroPlastic = 0;
+    }
+
+    addReagent(reagent) {
+        this.visibileMicroPlastic = Math.floor(this.numMicroPlastic * reagent.detection);
+    }
+}
+
+const tabWater = new Liquid(40, 50);
+const barleyTea = new Liquid(15, 25);
+const distilledWater = new Liquid(5, 8);
+
+const liquids = [tabWater, barleyTea, distilledWater];
+
+console.log(tabWater.numMicroPlastic, barleyTea.numMicroPlastic, distilledWater.numMicroPlastic)
+
+for (let liquid of liquids) {
+    liquid.addReagent(btb)
+}
+
+console.log(tabWater.visibileMicroPlastic, barleyTea.visibileMicroPlastic, distilledWater.visibileMicroPlastic)
+
+
+
+
+
+
+
+
+
+
