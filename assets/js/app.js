4ff9a37c68b4a77304863c0f253dfe65f3bfc3c3
Jiseung Jung <roadrunner23@yonsei.ac.kr>
2021-07-22 17:39:05 +0900

    make petry dish color change

--- assets/js/app.js
+++ assets/js/app.js
@@ -103,7 +103,7 @@ addEventListener('click', e => {
             if (AABB(rect1, rect2)) {
                 pipette.active = true
                 gsap.timeline()
-                    .set(liquid, { fill: reagent.color })
+                    .set(liquid, { attr: { fill: reagent.color } })
                     .to(pipette, 0.2, { x: (rect1.left + rect1.right) / 2, y: rect1.top, rotate: -45 })
                     .to(liquid, {
                         attr: { y: 113 }, onComplete: () => {
@@ -120,6 +120,7 @@ addEventListener('click', e => {
             const pipetteLiquid = pipette.contentDocument.querySelector('#liquid');
 
             if (AABB(rect1, rect2)) {
+                console.log(pipetteLiquid.getAttribute('fill'))
                 pipette.active = true
                 gsap.timeline()
                     .to(pipette, 0.2, { x: (rect1.left + rect1.right + rect2.width) / 2, y: (rect1.top + rect1.bottom - rect2.height) / 2 })
@@ -128,7 +129,8 @@ addEventListener('click', e => {
                             pipette.active = false
                             pipette.full = false
                         }
-                    })
+                    }, 0.2)
+                    .to(petryDish.liquid, { attr: { fill: pipetteLiquid.getAttribute('fill') } }, 0.2)
             }
         }
 })
