84199b99a8938a34bfeab20388651dbaf2d659ed
Jiseung Jung <roadrunner23@yonsei.ac.kr>
2021-07-22 17:29:21 +0900

    drop pipette liquid to petry dishes

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,12 +13,12 @@ class Reagent {
         document.querySelector("#test-tubes").appendChild(object);
 
         object.addEventListener('load', () => {
+            this.object = object
             this.svg = object.contentDocument.querySelector('svg');
             this.liquid = this.svg.querySelector('#liquid');
             this.setColor(color);
             this.animate();
             this.setY(300);
-            this.object = object
         })
     }
 
@@ -49,7 +49,7 @@ const btb = new Reagent("Bromothymol Blue", "green", 0.9);
 const ppt = new Reagent("Phenolphthalein", "white", 0.2);
 const phenolRed = new Reagent("Phenol Red", "red", 0.3);
 
-class Liquid {
+class PetryDish {
     constructor(min, max) {
         this.numMicroPlastic = Math.floor((max - min) * Math.random() + min);
         this.visibileMicroPlastic = 0;
@@ -65,21 +65,26 @@ class Liquid {
         object.setAttribute("data", "assets/svg/petriDish.svg");
         object.setAttribute("type", "image/svg+xml");
         document.querySelector("#petri-dishes").appendChild(object);
+
+        object.addEventListener('load', () => {
+            this.object = object
+            this.svg = object.contentDocument.querySelector('svg');
+            this.liquid = this.svg.querySelector('#liquid');
+        })
     }
 }
 
-const tabWater = new Liquid(40, 50);
-const barleyTea = new Liquid(15, 25);
-const distilledWater = new Liquid(5, 8);
+const tabWater = new PetryDish(40, 50);
+const barleyTea = new PetryDish(15, 25);
+const distilledWater = new PetryDish(5, 8);
 
 const reagents = [thymolBlue, methylOrange, wrightStein, btb, ppt, phenolRed]
-
-const liquids = [tabWater, barleyTea, distilledWater];
+const petryDishes = [tabWater, barleyTea, distilledWater];
 
 console.log(tabWater.numMicroPlastic, barleyTea.numMicroPlastic, distilledWater.numMicroPlastic)
 
-for (let liquid of liquids) {
-    liquid.addReagent(btb)
+for (let petryDish of petryDishes) {
+    petryDish.addReagent(btb)
 }
 
 console.log(tabWater.visibileMicroPlastic, barleyTea.visibileMicroPlastic, distilledWater.visibileMicroPlastic)
@@ -89,26 +94,43 @@ addEventListener('mousemove', e => {
 })
 
 addEventListener('click', e => {
-    if (pipette.full) return
+    if (!pipette.full)
+        for (const reagent of reagents) {
+            const rect1 = reagent.object.getBoundingClientRect()
+            const rect2 = pipette.getBoundingClientRect()
+            const liquid = pipette.contentDocument.querySelector('#liquid');
 
-    for (const reagent of reagents) {
-        const rect1 = reagent.object.getBoundingClientRect()
-        const rect2 = pipette.getBoundingClientRect()
-        const liquid = pipette.contentDocument.querySelector('#liquid');
-
-        if (AABB(rect1, rect2)) {
-            pipette.active = true
-            gsap.timeline()
-                .set(liquid, { fill: reagent.color })
-                .to(pipette, 0.2, { x: (rect1.left + rect1.right) / 2, y: rect1.top, rotate: -45 })
-                .to(liquid, {
-                    attr: { y: 113 }, onComplete: () => {
-                        pipette.active = false
-                        pipette.full = true
-                    }
-                })
+            if (AABB(rect1, rect2)) {
+                pipette.active = true
+                gsap.timeline()
+                    .set(liquid, { fill: reagent.color })
+                    .to(pipette, 0.2, { x: (rect1.left + rect1.right) / 2, y: rect1.top, rotate: -45 })
+                    .to(liquid, {
+                        attr: { y: 113 }, onComplete: () => {
+                            pipette.active = false
+                            pipette.full = true
+                        }
+                    })
+            }
         }
-    }
+    else
+        for (const petryDish of petryDishes) {
+            const rect1 = petryDish.object.getBoundingClientRect()
+            const rect2 = pipette.getBoundingClientRect()
+            const pipetteLiquid = pipette.contentDocument.querySelector('#liquid');
+
+            if (AABB(rect1, rect2)) {
+                pipette.active = true
+                gsap.timeline()
+                    .to(pipette, 0.2, { x: (rect1.left + rect1.right + rect2.width) / 2, y: (rect1.top + rect1.bottom - rect2.height) / 2 })
+                    .to(pipetteLiquid, {
+                        attr: { y: 283 }, onComplete: () => {
+                            pipette.active = false
+                            pipette.full = false
+                        }
+                    })
+            }
+        }
 })
 
 function AABB(rect1, rect2) {
